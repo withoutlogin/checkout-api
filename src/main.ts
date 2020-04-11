0a661498e2fadcfc4419aa6a6e0aeb4e8f23e73b
Szymon Kumorek <szymon.kumorek@gmail.com>
2020-04-11 21:01:12 +0200

    Project initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+// import 'source-map-support/register';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+
+  await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
