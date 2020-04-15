7aa6261c9ebb5939dc77c84ed7c3cc3d18712785
Szymon Kumorek <szymon.kumorek@gmail.com>
2020-04-16 01:07:03 +0200

    feat(cart-product): add PUT method for quantity update

--- src/cart/controllers/cart-product.controller.ts
+++ src/cart/controllers/cart-product.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Post,
   UseInterceptors,
+  Put,
 } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { ApiParam, ApiResponse } from '@nestjs/swagger';
@@ -20,6 +21,9 @@ import {
 } from '../cart-read-stack/dto/cart-products.dto';
 import { CartByIdQuery } from '../cart-read-stack/queries/cart-by-id.query';
 import { CartProductInputDto } from './dtos/cart-product.dto';
+import { CartProductUpdateInputDto } from './dtos/cart-product-update-input.dto';
+import { ProductQuantityUpdatedEvent } from '../cart-domain/events/product-quantity-updated-event';
+import { ChangeProductQuantityCommand } from '../cart-domain/commands/change-product-quantity.command';
 
 @Controller('cart/:cartId/products')
 @UseInterceptors(CreatedLocationInterceptor)
@@ -83,5 +87,34 @@ export class CartProductController {
       throw new NotFoundException();
     }
     return product;
+  }
+
+  @Put(':productId')
+  async updateProductInCart(
+    @Param('cartId') cartId: string,
+    @Param('productId') productId: string,
+    @Body() productUpdateInput: CartProductUpdateInputDto,
+  ): Promise<void> {
+    const cart = await this.queryBus.execute(new CartByIdQuery(cartId));
+    if (!cart) {
+      throw new NotFoundException();
+    }
+
+    const productsDto = (await this.queryBus.execute(
+      new CartProductsQuery(cartId),
+    )) as CartProductsReadDto;
+
+    const product = productsDto.getProduct(productId);
+    if (!product) {
+      throw new NotFoundException();
+    }
+
+    await this.commandBus.execute(
+      new ChangeProductQuantityCommand(
+        cartId,
+        productId,
+        productUpdateInput.quantity,
+      ),
+    );
   }
 }
