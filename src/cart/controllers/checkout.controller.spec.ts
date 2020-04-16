215e26fb8a76c95cc709f8403f343a3575c327e4
Szymon Kumorek <szymon.kumorek@gmail.com>
2020-04-16 09:04:05 +0200

    test: add CheckoutController basic test

--- src/cart/controllers/checkout.controller.spec.ts
+++ src/cart/controllers/checkout.controller.spec.ts
@@ -1,19 +1,19 @@
 import { CqrsModule, QueryBus } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CartController } from './cart.controller';
+import { CheckoutController } from './checkout.controller';
 
-describe('Cart Controller', () => {
+describe('Checkout Controller', () => {
   // todo tests for controller
-  let controller: CartController;
+  let controller: CheckoutController;
   let queryBus: QueryBus;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [CqrsModule],
-      controllers: [CartController],
+      controllers: [CheckoutController],
     }).compile();
 
-    controller = module.get<CartController>(CartController);
+    controller = module.get<CheckoutController>(CheckoutController);
     queryBus = module.get<QueryBus>(QueryBus);
   });
 
@@ -21,16 +21,4 @@ describe('Cart Controller', () => {
     expect(controller).toBeDefined();
     expect(queryBus).toBeDefined();
   });
-
-  // it('should execute CartByIdQuery', async (done) => {
-  //   // having
-  //   const someCartId = uuidv4();
-  //   queryBus.execute = jest.fn().mockReturnValue(Promise.resolve());
-
-  //   // expect
-  //   expect(queryBus.execute).toBeCalledWith(new CartByIdQuery(someCartId));
-
-  //   // when
-  //   controller.getCart(someCartId);
-  // });
 });
